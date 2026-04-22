874c16ae002dadb6c01676f873dfe4ab3a070c8d
Yoni190 <yfisuhu@gmail.com>
2026-04-22 23:19:15 +0300

    feat: add f_name and l_name to users table

---
+++ database/migrations/2026_04_22_201621_add_first_name_and_last_name_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('f_name')->after('id');
+            $table->string('l_name')->after('f_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('f_name');
+            $table->dropColumn('l_name');
+        });
+    }
+};
