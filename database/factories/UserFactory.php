f1df29f1a8d00a5de7667a4f65677361788bdbe5
Yoni190 <yfisuhu@gmail.com>
2026-04-23 14:51:54 +0300

    fix: add f_name and l_name to user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'f_name' => fake()->name(),
+            'l_name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
