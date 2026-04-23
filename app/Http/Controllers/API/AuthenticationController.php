56e4a33489dd4dbb8d5e25201eec85b997307fe0
Yoni190 <yfisuhu@gmail.com>
2026-04-23 14:01:19 +0300

    feat: create register function

---
+++ app/Http/Controllers/API/AuthenticationController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthenticationController extends Controller
+{
+    public function register(Request $request) {
+        $request->validate([
+            'f_name' => 'required',
+            'l_name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ]);
+
+        User::create([
+            'f_name' => $request->f_name,
+            'l_name' => $request->l_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json([ 'message' => 'User registered successfully' ]);
+    }
+}
