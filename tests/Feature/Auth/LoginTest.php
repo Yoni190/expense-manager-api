68a3310e1d9f09fd146eb2032e6e0e48adbef062
Yoni190 <yfisuhu@gmail.com>
2026-04-23 15:05:05 +0300

    chore: test user login with invalid credentials

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -37,6 +37,21 @@ class LoginTest extends TestCase
         $response->assertStatus(422);
 
         $response->assertJsonValidationErrors(['email', 'password']);
-        
+    }
+
+    public function test_user_login_with_invalid_credentials () {
+        User::factory()->create([
+            'email' => 'johndoe@gmail.com',
+            'password' => '123456'
+        ]);
+
+        $response = $this->postJson('api/login', [
+            'email' => 'johnsmith@gmail.com',
+            'password' => '123456'
+        ]);
+
+        $response->assertStatus(401);
+
+        $response->assertJson(['message' => 'Invalid Credentials']);
     }
 }
