aab1dea6ea1e4858b7005c9468d0d787fa70ca75
Yoni190 <yfisuhu@gmail.com>
2026-04-24 19:00:23 +0300

    chore: add route to delete categories

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::get('categories', [CategoryController::class, 'index']);
     Route::post('categories', [CategoryController::class, 'store']);
     Route::put('categories/{category}', [CategoryController::class, 'update']);
+    Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
 });
