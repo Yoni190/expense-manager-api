67f5ef4b3e8f690b6d30ea310114aa9e7f19e263
Yoni190 <yfisuhu@gmail.com>
2026-04-24 18:45:43 +0300

    chore: create category factory

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Database\Factories\CategoryFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
+
+    use HasFactory;
+
     protected $fillable = [
         'name',
         'user_id'
